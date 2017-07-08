cfc1ed9420cbbe00c80f762af227aac69c4411b1
jwpc <jw6lee@gmail.com>
2017-07-09 00:06:49 +0900

    start to solve...

---
+++ src/main/java/ourstudy/skt_test/answers/_Second_TaskOrdering.java
@@ -0,0 +1,25 @@
+package ourstudy.skt_test.answers;
+
+/**
+ * Created by jw on 2017-07-08.
+ * https://algospot.com/judge/problem/read/ORDERING
+ */
+public class _Second_TaskOrdering {
+}
+
+
+/*
+TEST CASE
+4
+4 0
+4 2
+AB
+CB
+4 1
+DC
+4 4
+DA
+BD
+CA
+CD
+ */
