08d553ad9d2fe958cf0a56f0b35f4d0f2787ff18
jwpc <jw6lee@gmail.com>
2017-08-07 01:52:48 +0900

    CLRS - counting sort hackerrank -count sort 1,2

---
+++ src/main/java/ourstudy/IntroductionToAlgorithms/ch8/CountingSort.java
@@ -0,0 +1,36 @@
+package ourstudy.IntroductionToAlgorithms.ch8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Created by jw on 2017-08-07.
+ * visualizing counting sort: http://www.cs.miami.edu/home/burt/learning/Csc517.091/workbook/countingsort.html
+ */
+public class CountingSort {
+
+    public int[] sort(int...array) {
+        int max = Arrays.stream(array).max().getAsInt();
+        int[] count = new int[max + 1];
+        int[] sorted = new int[array.length];
+        //init
+        IntStream.range(0, count.length).forEach(i -> count[i] = 0);
+        IntStream.range(0, sorted.length).forEach(i -> sorted[i] = 0);
+
+        //array의 원소값이 나타난 횟수 카운트
+        IntStream.range(0, array.length).forEach(i -> count[array[i]]++);
+
+        //count배열을 누적카운트배열로 바꿈.
+        IntStream.range(1, count.length).forEach(i -> {
+            count[i] = count[i - 1] + count[i];
+        });
+
+        for (int i : array) {
+            if (count[i] > 0) {
+                sorted[count[i] - 1] = i;
+                count[i]--;
+            }
+        }
+        return sorted;
+    }
+}
---
+++ src/main/java/ourstudy/IntroductionToAlgorithms/ch8/CountingSortClient.java
@@ -0,0 +1,16 @@
+package ourstudy.IntroductionToAlgorithms.ch8;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Created by jw on 2017-08-07.
+ */
+public class CountingSortClient {
+    public static void main(String[] args) {
+        int[] input = {5,4,3,2,1,0};
+        CountingSort cs = new CountingSort();
+        int[] arr = cs.sort(input);
+        System.out.println(Arrays.stream(arr).boxed().collect(Collectors.toList()));
+    }
+}
