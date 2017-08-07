d29e6397fe250bd204f70002b711e494bc09f706
jwpc <jw6lee@gmail.com>
2017-08-07 23:25:14 +0900

    radix sort completed

--- src/main/java/ourstudy/IntroductionToAlgorithms/ch8/RadixSort.java
+++ src/main/java/ourstudy/IntroductionToAlgorithms/ch8/RadixSort.java
@@ -1,9 +1,6 @@
 package ourstudy.IntroductionToAlgorithms.ch8;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -13,30 +10,29 @@ public class RadixSort {
 
 	public int[] sort(int[] array){
 		int k = String.valueOf(Arrays.stream(array).max().getAsInt()).length();
-		Queue q = new LinkedList<>();
-
 		for(int i = 0, exp = 1; i < k; i++, exp *= 10) {
-			int[] count = new int[10];
-			for (int j : array) {
-				count[j / exp % 10]++;
-			}
-			//count배열을 누적카운트배열로 바꿈.
-			IntStream.range(1, count.length).forEach(j -> {
-				count[j] = count[j - 1] + count[j];
-			});
-			System.out.println("array : " + Arrays.stream(array).boxed().collect(Collectors.toList()));
-			System.out.println("count : " + Arrays.stream(count).boxed().collect(Collectors.toList()));
-
-			int[] sorted = new int[array.length];
-			//TODO // FIXME: 2017. 8. 7. 
-			for (int j = array.length - 1; j >=0; j--) {
-				int pos = array[j] / exp % 10;
-				sorted[count[pos] - 1] = j;
-				count[pos]--;
-			}
-			array = sorted;
+			array = this.countSort(array, exp);
 		}
 
 		return array;
+	}
+
+	private int[] countSort(int[] array, int exp) {
+		int[] count = new int[10];
+		for (int j : array) {
+            count[j / exp % 10]++;
+        }
+		//count배열을 누적카운트배열로 바꿈.
+		IntStream.range(1, count.length).forEach(j -> {
+            count[j] = count[j - 1] + count[j];
+        });
+
+		int[] sorted = new int[array.length];
+		for (int j = array.length - 1; j >=0; j--) {
+            int pos = array[j] / exp % 10;
+            sorted[count[pos] - 1] = array[j];
+            count[pos]--;
+        }
+		return sorted;
 	}
 }
