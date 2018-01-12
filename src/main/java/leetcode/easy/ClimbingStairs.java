9e23bb93284d16f72ee8f8442149bbf1357a7cd9
jwlee1 <jwlee1@tmon.co.kr>
2018-01-12 18:06:21 +0900

    climbing staris solved

---
+++ src/main/java/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] ways = new int[n + 1];
+        ways[0] = 0;
+        ways[1] = 1;
+        ways[2] = 2;
+
+        for(int i = 3; i <=n ; i++){
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+        return ways[n];
+    }
+}
