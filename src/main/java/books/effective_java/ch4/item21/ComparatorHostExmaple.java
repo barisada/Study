3853323a81dce05ffe9aaf776449d318d0460a08
jw <jw6lee@gmail.com>
2018-02-22 02:44:13 +0900

    item20 : Use function objects to represent strategies

---
+++ src/main/java/books/effective_java/ch4/item21/ComparatorHostExmaple.java
@@ -0,0 +1,16 @@
+package books.effective_java.ch4.item21;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+public class ComparatorHostExmaple {
+
+    private static class StringComparatorExample implements Comparator<String>, Serializable {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.length() - o2.length();
+        }
+    }
+
+    public static final Comparator<String> STR_LENGTH_COMPARATOR = new StringComparatorExample();
+}
---
+++ src/main/java/books/effective_java/ch4/item21/StrategyForFunctionObjectTest.java
@@ -0,0 +1,31 @@
+package books.effective_java.ch4.item21;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class StrategyForFunctionObjectTest {
+    public static void main(String[] args) {
+        //concreate comparator example
+        System.out.println("======== concrete comparator ex ====");
+        String[] arr = {"b", "a", "ab", "abc", "bc"};;
+        Arrays.sort(arr, ComparatorHostExmaple.STR_LENGTH_COMPARATOR);
+        System.out.println("sort by length : " + Arrays.toString(arr));
+
+        Arrays.sort(arr, ComparatorHostExmaple.STR_LENGTH_COMPARATOR.reversed());
+        System.out.println("reverse sort by length : " + Arrays.toString(arr));
+
+
+        //anonymous example
+        System.out.println("======== anonymous comparator ex ====");
+        List<String> list = Arrays.asList(arr);
+        list.sort(Comparator.comparingInt(String::length));
+        System.out.println("sort by length : " + list);
+
+        list.sort(Collections.reverseOrder(Comparator.comparingInt(String::length)));
+        System.out.println("reverse sort by length : " + list);
+
+
+    }
+}
