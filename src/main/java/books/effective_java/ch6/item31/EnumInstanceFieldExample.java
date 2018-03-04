cac66386157e283692418aa88f51b4a838873028
jw <jw6lee@gmail.com>
2018-03-05 01:16:25 +0900

    Item 24: Eliminate unchecked warnings

--- src/main/java/books/effective_java/ch6/item31/EnumInstanceFieldExample.java
+++ src/main/java/books/effective_java/ch6/item31/EnumInstanceFieldExample.java
@@ -1,12 +1,12 @@
 package books.effective_java.ch6.item31;
 
-public enum EnumInstanceFieldExmaple {
+public enum EnumInstanceFieldExample {
     SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5),
     SEXTET(6), SEPTET(7), OCTET(8), NONET(9), DECTET(10),
     TRIPLE_QUARTET(12);
 
     private final int numOfMusician;
-    EnumInstanceFieldExmaple(int size){
+    EnumInstanceFieldExample(int size){
         this.numOfMusician = size;
     }
 
@@ -15,7 +15,7 @@ public enum EnumInstanceFieldExmaple {
     }
 
     public static void main(String[] args) {
-        for(EnumInstanceFieldExmaple ex : EnumInstanceFieldExmaple.values()){
+        for(EnumInstanceFieldExample ex : EnumInstanceFieldExample.values()){
             System.out.println(ex + " instance field value : " + ex.numberOfMusicians()
                     + " vs ordianl : " + ex.ordinal());
         }
