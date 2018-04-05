069b9ccb8d1c7f23e098dda20dae7dd82369c36f
jwpc <jw6lee@gmail.com>
2018-04-05 23:07:55 +0900

    ITEM 58: USE CHECKED EXCEPTIONS FOR RECOVERABLE CONDITIONS

--- src/main/java/books/effective_java/ch9/item58/ExceptionCaseByCaseExmaple.java
+++ src/main/java/books/effective_java/ch9/item58/ExceptionCaseByCaseExmaple.java
@@ -1,5 +1,7 @@
 package books.effective_java.ch9.item58;
 
+import java.util.Random;
+
 /**
  * Created on 2018-04-05.
  */
@@ -14,12 +16,28 @@ public class ExceptionCaseByCaseExmaple {
         }
     }
 
+    public static void calcChanges(double itemPrice) throws NotEnoughMoney{
+        double remain = new Random().nextInt(1) / 1.0;
+        if(itemPrice > remain) throw new NotEnoughMoney("not enough money : $" + (itemPrice - remain));
+    }
+
     public static void uncheckedError(){
         throw new StupidRuntimeException("So Stupid");
     }
+
 }
 
 class StupidException extends Exception{
+}
+
+class NotEnoughMoney extends Exception{
+    public NotEnoughMoney() {
+        super();
+    }
+
+    public NotEnoughMoney(String message) {
+        super(message);
+    }
 }
 
 class StupidRuntimeException extends RuntimeException{
--- src/main/java/books/effective_java/ch9/item58/ExceptionCaseByCaseTest.java
+++ src/main/java/books/effective_java/ch9/item58/ExceptionCaseByCaseTest.java
@@ -4,8 +4,9 @@ package books.effective_java.ch9.item58;
  * Created on 2018-04-05.
  */
 public class ExceptionCaseByCaseTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotEnoughMoney {
         ExceptionCaseByCaseExmaple.checkedError();
+        ExceptionCaseByCaseExmaple.calcChanges(Math.random());
         ExceptionCaseByCaseExmaple.uncheckedError();
     }
 }
