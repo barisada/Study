13d148bfe11a2e62910918eea460ddbf1c1d3565
jw <jw6lee@gmail.com>
2018-07-23 18:41:23 -0400

    CTCI : hackerrank - DFS connected cell in a grid

---
+++ src/main/java/hackerrank/interviewKit/DFS_ConnectedCellInaGrid.java
@@ -0,0 +1,42 @@
+package hackerrank.interviewKit;
+
+public class DFS_ConnectedCellInaGrid {
+    static boolean[][] visit;
+    // Complete the maxRegion function below.
+    static int maxRegion(int[][] grid) {
+        int max = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                max = Math.max(max, dfs(grid, i, j));
+            }
+        }
+        return max;
+    }
+
+    static int dfs(int[][] grid, int i, int j){
+        if(i < 0
+                || j < 0
+                || i >= grid.length
+                || j >= grid[i].length
+
+                ) return 0;
+
+        if(!visit[i][j]){
+            visit[i][j] = true;
+            if(grid[i][j] > 0){
+
+                return 1 + dfs(grid, i, j+1) +
+                        dfs(grid, i + 1, j) +
+                        dfs(grid, i + 1, j + 1) +
+                        dfs(grid, i - 1, j) +
+                        dfs(grid, i, j - 1) +
+                        dfs(grid, i - 1, j - 1) +
+                        dfs(grid, i -1, j + 1) +
+                        dfs(grid, i + 1, j - 1)
+                        ;
+            }
+        }
+
+        return 0;
+    }
+}
