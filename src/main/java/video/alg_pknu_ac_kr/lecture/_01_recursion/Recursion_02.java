f4702d54bfa2b0b2a089d6c0f55f2253972ede37
jw <jw6lee@gmail.com>
2018-07-27 16:54:33 -0400

    algorithm lecture

---
+++ src/main/java/video/alg_pknu_ac_kr/lecture/_01_recursion/Recursion_02.java
@@ -0,0 +1,110 @@
+package video.alg_pknu_ac_kr.lecture._01_recursion;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * Recursive Thinking
+ * 순환적 사고
+ */
+public class Recursion_02 {
+    public static void main(String[] args) {
+        String str = "abcdef";
+        int length = stringLength(str);
+        System.out.println(str + " length = " + length);
+
+        int length2 = stringLengthUsingStack(str);
+        System.out.println(str + " length (using stack) = " + length2);
+
+        System.out.print(str + "을 출력한다 : "); printChars(str);
+        System.out.print(str + " 을 역으로 출력한다: ");printCharsReverse(str);
+        System.out.println();
+
+        int n = 7;
+        System.out.print(n + "을 바이너리로 출력한다."); printBinary(n);
+        System.out.println();
+
+        int[] data = {1,2,3,4,5};
+        System.out.println(Arrays.toString(data) + "의 합을 출력한다. : " + sum(data.length, data));
+    }
+
+    /**
+     * 스트링 길이를 구하는 메소드.
+     * string length = n 일때
+     * n = n -1
+     * @param str
+     * @return
+     */
+    private static int stringLength(String str) {
+        if(str.equals("")){
+            return 0;
+        }
+        return 1 + stringLength(str.substring(1));
+    }
+
+    private static int stringLengthUsingStack(String str) {
+        Stack<Integer> stack = new Stack<>();
+        while(!str.equals("")){
+            stack.push(1);
+            str = str.substring(1);
+        }
+
+        int count = 0;
+        while(!stack.isEmpty()){
+            count += stack.pop();
+        }
+
+        return count;
+    }
+
+    /**
+     * 주어진 스트링을 출력하는 메소드
+     * recursion방식으로...
+     * @param str
+     */
+    private static void printChars(String str) {
+        if(stringLength(str) == 0) {
+            System.out.println();
+            return;
+        }
+        System.out.print(str.charAt(0));
+        printChars(str.substring(1));
+    }
+
+    /**
+     * 주어진 스트링을 거꾸로 출력하는 메소드
+     * @param str
+     */
+    private static void printCharsReverse(String str) {
+        if(stringLength(str) == 0) {
+            return;
+        }
+        printCharsReverse(str.substring(1));
+        System.out.print(str.charAt(0));
+    }
+
+    /**
+     * 양수 숫자 n을 이진수로 출력한다.
+     * @param n
+     */
+    private static void printBinary(int n) {
+        if(n < 2){
+            System.out.print(n);
+        } else {
+            printBinary(n / 2);
+            System.out.print( n % 2);
+        }
+    }
+
+    /**
+     * data[0]에서 data[n - 1] 까지의 합을 구하는 메소드
+     * @param n
+     * @param data
+     */
+    private static int sum(int n, int[] data){
+        if(n == 0) return 0;
+        return data[n - 1] + sum(n - 1, data);
+
+    }
+
+}
---
+++ src/main/java/video/alg_pknu_ac_kr/lecture/_01_recursion/Recursion_03.java
@@ -0,0 +1,178 @@
+package video.alg_pknu_ac_kr.lecture._01_recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Designing Recursion
+ * 순환 알고리즘의 설계
+ */
+public class Recursion_03 {
+    public static void main(String[] args) {
+        int[] data = {99,100,2,3,1,6,7,9,0,11,22,32,25,44,66,8,12,13,200,15};
+        int begin = 0;
+        int end = data.length -1;
+        int target1  = 32;
+
+        System.out.println("암시적 순차검색 >>> " + Arrays.toString(data) + "에서 " +  target1 + "을 찾는다 : "
+                + implicitSequentialSearch(data, end, target1));
+
+        int target2 = 8;
+        System.out.println("명시적 순차검색 >>> " + Arrays.toString(data) + "에서 " +  target2 + "을 찾는다 : "
+                + explicitSequentialSearch(data, begin, end, target2));
+
+        System.out.println("명시적 순차검색 뒤에서부터 >>> " + Arrays.toString(data) + "에서 " +  target2 + "을 찾는다 : "
+                + explicitSequentialSearchAlternative(data, begin, end, target2));
+
+        System.out.println("명시적 순차검색 중간부터 >>> " + Arrays.toString(data) + "에서 " +  target2 + "을 찾는다 : "
+                + explicitSequentialSearchAlternative2(data, begin, end, target2));
+
+        System.out.println("최대값 찾기 >>> " + Arrays.toString(data) + "에서 최대값을 찾는다 : "
+                + findMax(data, begin, end));
+
+        System.out.println("최대값 찾기 반반 나눠서 찾기>>> " + Arrays.toString(data) + "에서 최대값을 찾는다 : "
+                + findMaxAlternative(data, begin, end));
+
+        Arrays.sort(data);
+        int target3 = 99;
+        System.out.println("이진검색 >>> " + Arrays.toString(data) + " 에서 "  + target3 + " 을 찾는다 : "
+                + binarySearch(data, begin, end, target3));
+    }
+
+    /**
+     * data size = n 일때
+     * data[0]부터 data[n-1]까지 사이에서 target을 검색한다.
+     * 없으면 -1을 리턴한다.
+     * @param data
+     * @param end
+     * @param target
+     * @return
+     */
+    private static String implicitSequentialSearch(int[] data, int end, int target) {
+        for(int i = 0; i <= end; i++){
+            if(data[i] == target)
+                return "Found at data[" + i + "]";
+        }
+        return "Not Found";
+    }
+
+    /**
+     * 명시적 순차 검색
+     * data size = n 일때
+     * data[0]부터 data[n-1]까지 사이에서 target을 검색한다.
+     * 없으면 -1을 리턴한다.
+     * @param data
+     * @param begin
+     * @param end
+     * @param target
+     * @return
+     */
+    private static String explicitSequentialSearch(int[] data, int begin, int end, int target) {
+        if(begin > end){
+            return "Not Found";
+        } else if(data[end] == target){
+            return "Found at data[" + end + "]";
+        } else {
+            return explicitSequentialSearch(data, begin, end - 1, target);
+        }
+    }
+
+    /**
+     * 뒤에서 부터 검색
+     * @param data
+     * @param begin
+     * @param end
+     * @param target
+     * @return
+     */
+    private static String explicitSequentialSearchAlternative(int[] data, int begin, int end, int target) {
+        if(begin > end){
+            return "Not Found";
+        } else if(data[begin] == target){
+            return "Found at data[" + begin + "]";
+        } else {
+            return explicitSequentialSearch(data, begin + 1, end, target);
+        }
+    }
+
+    /**
+     * 중간 먼저 검색
+     * 없으면 0 ~ 중간 검색
+     * 없으면 중간 ~ 마지막 검색
+     * 한번씩 다볼때까지 검색
+     * @param data
+     * @param begin
+     * @param end
+     * @param target
+     * @return
+     */
+    private static String explicitSequentialSearchAlternative2(int[] data, int begin, int end, int target) {
+        if(begin > end){
+            return "Not Found";
+        } else {
+            int mid = (begin + end) / 2;
+            if(data[mid] == target){
+                return "Found at data[" + mid + "]";
+            } else {
+                String result = explicitSequentialSearchAlternative2(data, begin, mid - 1, target);
+                if(result.equals("Not Found")){
+                    return explicitSequentialSearch(data, mid + 1, end, target);
+                } else {
+                    return result;
+                }
+            }
+        }
+    }
+
+    /**
+     * data[begin]에서 data[end] 사이의 최대값을 찾는다.
+     * 예) n > end 일떄
+     * data[n] 과 data[n + 1] ~ data[end]사이의 최대값을 비교해서
+     * 둘중에 큰값을 리턴하면된다.
+     *
+     * data에는 1개 이상의 데이터가 있다고 가정.
+     * @param data
+     * @param begin
+     * @param end
+     * @return
+     */
+    private static int findMax(int[] data, int begin, int end) {
+        if(begin == end){
+            return data[begin];
+        } else {
+            return Math.max(data[begin], findMax(data, begin + 1, end));
+        }
+    }
+
+    private static int findMaxAlternative(int[] data, int begin, int end) {
+        if(begin == end){
+            return data[begin];
+        } else {
+            int mid = (begin + end) / 2;
+            return Math.max(findMax(data, begin, mid), findMax(data, mid + 1, end));
+        }
+    }
+
+    /**
+     * data[begin]와 data[end] 사이에서 target을 찾는다.
+     * @param data
+     * @param begin
+     * @param end
+     * @param target
+     * @return
+     */
+    private static String binarySearch(int[] data, int begin, int end, int target){
+        if(begin > end){
+            return "Not Found";
+        } else {
+            int mid = (begin + end) / 2;
+            if(data[mid] == target){
+                return "Found at data[" + mid + "]";
+            } else if(data[mid] < target){
+                return binarySearch(data, mid + 1, end, target);
+            } else {
+                return binarySearch(data, begin, mid - 1, target);
+            }
+        }
+    }
+}
