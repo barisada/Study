56a088ee9b22b13105232442d56b53e55c7017f6
jw <jw6lee@gmail.com>
2018-07-27 19:07:59 -0400

    algorithm lecture

---
+++ src/main/java/video/alg_pknu_ac_kr/lecture/_01_recursion/Recursion_04_Maze.java
@@ -0,0 +1,82 @@
+package video.alg_pknu_ac_kr.lecture._01_recursion;
+
+import java.util.Arrays;
+
+/**
+ * MAZE
+ * 현재 위치에서 출구까지 있는 경로가 있으려면,
+ *  1. 현재 위치가 출구
+ *  2. 이웃한 셀(왼쪽, 오른쪽, 위, 아래)들중 하나에서
+ *  현재 위치를 지나치지 않고 출구까지 가는 경우
+ */
+public class Recursion_04_Maze {
+    private static int EXIT = 9;
+    private static int WALL = 0;
+    private static int PATH_WAY = 1;
+    private static int PATH_TO_DEAD_END = 4;
+    private static int PATH_TO_EXIT = 7;
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[][] grid = createGrid(n);
+        boolean hasFound = findPathDecision(grid, 0, 0);
+
+        System.out.println("미로에서 나가는 출구가 있나? : " + hasFound);
+        for(int[] row : grid){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+
+    private static boolean findPathDecision(int[][] grid, int row, int col) {
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid.length   //좌표가 범위안에 있는지 확인
+                || (grid[row][col] !=PATH_WAY && grid[row][col] != EXIT)
+                ){
+            return false;
+        } else if(grid[row][col] == EXIT){
+            grid[row][col] = PATH_TO_EXIT;
+            return true;
+        } else {
+            grid[row][col] = PATH_TO_EXIT;
+
+            //북-동-남-서 순으로 체크.
+            boolean result = findPathDecision(grid,row - 1, col)
+                    || findPathDecision(grid, row, col + 1)
+                    ||findPathDecision(grid, row +1, col)
+                    || findPathDecision(grid, row, col - 1)
+                    ;
+
+            if(result) {
+                return result;
+            }
+            else {
+                grid[row][col] = PATH_TO_DEAD_END;
+                return false;
+            }
+        }
+    }
+
+
+    private static int[][] createGrid(int n) {
+        int[] row0 = {1,1,1,1,1,1,1,0};
+        int[] row1 = {1,0,0,1,0,0,1,0};
+        int[] row2 = {1,1,1,0,1,1,1,0};
+        int[] row3 = {1,0,1,1,0,0,1,1};
+        int[] row4 = {1,0,0,0,1,1,0,0};
+        int[] row5 = {1,0,1,1,1,0,1,0};
+        int[] row6 = {1,1,1,0,1,1,1,0};
+        int[] row7 = {1,0,0,0,1,0,1,9};
+
+        int[][] grid = new int[n][n];
+        grid[0] = row0;
+        grid[1] = row1;
+        grid[2] = row2;
+        grid[3] = row3;
+        grid[4] = row4;
+        grid[5] = row5;
+        grid[6] = row6;
+        grid[7] = row7;
+
+        return grid;
+    }
+}
---
+++ src/main/java/video/alg_pknu_ac_kr/lecture/_01_recursion/Recursion_05_Counting_cells_in_a_Blob.java
@@ -0,0 +1,84 @@
+package video.alg_pknu_ac_kr.lecture._01_recursion;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Binary Image 에서
+ * 각 픽셀은 background pixel or imange pixcel.
+ * Connected image cell = Blob.
+ *
+ * Connected image cell is, if
+ *  1. 상하좌우 및 대각선으로 연결되있으면 connected cell.
+ */
+public class Recursion_05_Counting_cells_in_a_Blob {
+    private static final int BACKGROUND_CELL = 0;
+    private static final int IMAGE_CELL= 1;
+    private static final int MARKED_IMAGE_CELL = 2;
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[][] grid = createGrid(n);
+
+        int colNum = new Random().nextInt(n);
+        int rowNum = new Random().nextInt(n);
+
+        int blobSize = countBlobSize(grid, rowNum, colNum);
+        System.out.println("(" + colNum + ", " + rowNum + ")" + "의 Blob 사이즈는 " + blobSize + " 입니다.");
+        for(int[] row : grid){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    /**
+     * 입력 : grid 와 좌표 (col, row)
+     *
+     * 리턴 : 픽셀(col, row) 가 포함된 blob의 크기.
+     * (col, row)가 어떤  blob에도 속하지 않은 경우에 0.
+     * @param grid
+     * @param row
+     * @param col
+     * @return
+     */
+    private static int countBlobSize(int[][] grid, int row, int col) {
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid.length){
+            return 0;
+        } else if(grid[row][col] != IMAGE_CELL){
+            return 0;
+        } else {
+            grid[row][col] = MARKED_IMAGE_CELL;
+            //북-북동-동-동남-남-남서-서-북서 순으로 확인한다.
+            return 1 + countBlobSize(grid, row -1, col)
+                    + countBlobSize(grid, row -1 , col + 1)
+                    + countBlobSize(grid, row, col + 1)
+                    + countBlobSize(grid, row + 1, col + 1)
+                    + countBlobSize(grid, row + 1, col)
+                    + countBlobSize(grid, row + 1, col - 1)
+                    + countBlobSize(grid, row, col - 1)
+                    + countBlobSize(grid, row - 1, col -1);
+        }
+    }
+
+    private static int[][] createGrid(int n) {
+        int[] row0 = {1,0,0,0,0,0,0,1};
+        int[] row1 = {0,1,1,0,0,1,0,0};
+        int[] row2 = {1,1,0,0,1,0,1,0};
+        int[] row3 = {0,0,0,0,0,1,0,0};
+        int[] row4 = {0,1,0,1,0,1,0,0};
+        int[] row5 = {0,1,0,1,0,1,0,0};
+        int[] row6 = {1,0,0,0,1,0,0,1};
+        int[] row7 = {0,1,1,0,0,1,1,1};
+
+        int[][] grid = new int[n][n];
+        grid[0] = row0;
+        grid[1] = row1;
+        grid[2] = row2;
+        grid[3] = row3;
+        grid[4] = row4;
+        grid[5] = row5;
+        grid[6] = row6;
+        grid[7] = row7;
+
+        return grid;
+    }
+}
