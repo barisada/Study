4b6696df649c28f9829028fdf03f31ad73ecddb1
jw <jw6lee@gmail.com>
2018-12-15 15:11:57 -0500

    dailycode problem#1

--- src/main/java/dailycoding/Problem_01.java
+++ src/main/java/dailycoding/Problem_01.java
@@ -1,6 +1,8 @@
 package dailycoding;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
@@ -12,9 +14,12 @@ import java.util.Arrays;
 public class Problem_01 {
     public static void main(String[] args) {
         int[] arr = {10, 15, 3, 7};
+        int[] test2 = {2,3,4,5,7};
         System.out.println(findValue(arr, 13));
+        System.out.println(findValue2(test2, 6));
     }
 
+    // o (n log n)
     static boolean findValue(int[] arr, int val){
         Arrays.sort(arr);
         int lo = 0, hi = arr.length - 1;
@@ -27,6 +32,18 @@ public class Problem_01 {
             } else {
                 lo++;
             }
+        }
+        return false;
+    }
+    //time : O(n), space : o(n)
+    static boolean findValue2(int[] arr, int val){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            int target = val - arr[i];
+            if(map.containsKey(target) && map.get(target) != i){
+                return true;
+            }
+            map.put(arr[i], i);
         }
         return false;
     }
