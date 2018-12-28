aada2450ef0c42e2abe90bebaa491e02cf6fdfb8
Junwoo Lee <jlee@jonahgroup.com>
2018-12-28 17:42:19 -0500

    daily coding #22

---
+++ src/main/java/dailycoding/Problem_22.java
@@ -0,0 +1,48 @@
+package dailycoding;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list.
+ * If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
+ *
+ * For example, given the set of words 'quick', 'brown', 'the', 'fox', and the string "thequickbrownfox",
+ * you should return ['the', 'quick', 'brown', 'fox'].
+ *
+ * Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond",
+ * return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+ *
+ * leetcode #139
+ */
+public class Problem_22 {
+    public static void main(String[] args) {
+        Problem_22 test = new Problem_22();
+        List<String> list = Arrays.asList("quick", "brown", "the", "fox");
+        System.out.println("should be true : " + test.wordBreak("thequickbrownfox", list));
+
+        List<String> list2 = Arrays.asList("bed", "bath", "bedbath", "and", "beyond");
+        System.out.println("should be true : " + test.wordBreak("bedbathandbeyond", list2));
+
+        List<String> list3 = Arrays.asList("aaaa", "aaa");
+        System.out.println("should be true : " + test.wordBreak("aaaaaaa", list3));
+
+        List<String> list4 = Arrays.asList("go", "goal", "goals", "special");
+        System.out.println("should be true : " + test.wordBreak("goalspecial", list4));
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && words.contains(s.substring(j, i)))
+                    dp[i] = true;
+            }
+        }
+        return dp[s.length()];
+    }
+}
