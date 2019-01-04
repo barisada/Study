bfdec42485c817d0ec37e8474cdf32d6401bee26
Junwoo Lee <jlee@jonahgroup.com>
2019-01-04 17:40:35 -0500

    daily coding #29

---
+++ src/main/java/dailycoding/Problem_29.java
@@ -0,0 +1,39 @@
+package dailycoding;
+
+/**
+ * Run-length encoding is a fast and simple method of encoding strings.
+ * The basic idea is to represent repeated successive characters as a single count and character.
+ * For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+ *
+ * Implement run-length encoding and decoding.
+ * You can assume the string to be encoded have no digits and consists solely of alphabetic characters.
+ * You can assume the string to be decoded is valid.
+ */
+public class Problem_29 {
+    public static void main(String[] args) {
+        Problem_29 test = new Problem_29();
+        String result = test.encoding("AAAABBBCCDAA");
+        System.out.println(String.format("should be 4A3B2C1D2A : %s (%s)", result, "4A3B2C1D2A".equalsIgnoreCase(result)));
+
+        result = test.encoding("aaaabbbccc");
+        System.out.println(String.format("should be 4a3b3c : %s (%s)", result, "4a3b3c".equalsIgnoreCase(result)));
+
+        result = test.encoding("abbbcdddd");
+        System.out.println(String.format("should be 1a3b1c4d : %s (%s)", result, "1a3b1c4d".equalsIgnoreCase(result)));
+
+        result = test.encoding("wwwwaaadexxxxxxywww");
+        System.out.println(String.format("should be 4w3a1d1e6x1y3w : %s (%s)", result, "4w3a1d1e6x1y3w".equalsIgnoreCase(result)));
+    }
+
+    public String encoding(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length() - 1; i++){
+            int j = i;
+            while(j < s.length() - 1 && s.charAt(j) == s.charAt(j + 1)) j++;
+            sb.append(j - i + 1);
+            sb.append(s.charAt(i));
+            i = j;
+        }
+        return sb.toString();
+    }
+}
