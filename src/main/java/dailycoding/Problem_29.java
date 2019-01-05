1751f7948f9da0715275734ccc05f84cc78748ee
jw <jw6lee@gmail.com>
2019-01-05 00:22:00 -0500

    practice

--- src/main/java/dailycoding/Problem_29.java
+++ src/main/java/dailycoding/Problem_29.java
@@ -14,15 +14,23 @@ public class Problem_29 {
         Problem_29 test = new Problem_29();
         String result = test.encoding("AAAABBBCCDAA");
         System.out.println(String.format("should be 4A3B2C1D2A : %s (%s)", result, "4A3B2C1D2A".equalsIgnoreCase(result)));
+        result = test.decoding("4A3B2C1D2A");
+        System.out.println(String.format("should be AAAABBBCCDAA : %s (%s)", result, "AAAABBBCCDAA".equalsIgnoreCase(result)));
 
         result = test.encoding("aaaabbbccc");
         System.out.println(String.format("should be 4a3b3c : %s (%s)", result, "4a3b3c".equalsIgnoreCase(result)));
+        result = test.decoding("4a3b3c");
+        System.out.println(String.format("should be aaaabbbccc : %s (%s)", result, "aaaabbbccc".equalsIgnoreCase(result)));
 
         result = test.encoding("abbbcdddd");
         System.out.println(String.format("should be 1a3b1c4d : %s (%s)", result, "1a3b1c4d".equalsIgnoreCase(result)));
+        result = test.decoding("1a3b1c4d");
+        System.out.println(String.format("should be abbbcdddd : %s (%s)", result, "abbbcdddd".equalsIgnoreCase(result)));
 
         result = test.encoding("wwwwaaadexxxxxxywww");
         System.out.println(String.format("should be 4w3a1d1e6x1y3w : %s (%s)", result, "4w3a1d1e6x1y3w".equalsIgnoreCase(result)));
+        result = test.decoding("4w3a1d1e6x1y3w");
+        System.out.println(String.format("should be wwwwaaadexxxxxxywww : %s (%s)", result, "wwwwaaadexxxxxxywww".equalsIgnoreCase(result)));
     }
 
     public String encoding(String s){
@@ -33,6 +41,18 @@ public class Problem_29 {
             sb.append(j - i + 1);
             sb.append(s.charAt(i));
             i = j;
+        }
+        return sb.toString();
+    }
+
+    public String decoding(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            int num = Integer.valueOf(s.substring(i, i + 1));
+            for(int j = 0; j < num; j++){
+                sb.append(s.charAt(i + 1));
+            }
+            i++;
         }
         return sb.toString();
     }
